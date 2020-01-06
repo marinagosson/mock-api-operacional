89f0b1e68fc1bc927c1e6143148ef7bae698a738
Marina Gosson <marina.gosson@gmail.com>
2020-01-06 09:12:59 -0300

    ajustes rota de loja

--- app/routes/loja.js
+++ app/routes/loja.js
@@ -15,6 +15,26 @@ loja.get("/", async (req, res) => {
         qtdDigitoProdutoPeso: 4,
         qtdDecimaisEtiquetaPeso: 0
       }
+    },
+    {
+      id: 2,
+      nome_fantasia: "SUPERMERCADO SIMARA LTDA",
+      endereco: "RUA EXP. ANTONIO CAETANO DE SOUZA FILHO",
+      url_image: null,
+      configuracaoProduto: {
+        qtdDigitoProdutoPeso: 4,
+        qtdDecimaisEtiquetaPeso: 0
+      }
+    },
+    {
+      id: 3,
+      nome_fantasia: "SUPERMERCADO SIMARA LTDA",
+      endereco: "RUA EXP. ANTONIO CAETANO DE SOUZA FILHO",
+      url_image: null,
+      configuracaoProduto: {
+        qtdDigitoProdutoPeso: 4,
+        qtdDecimaisEtiquetaPeso: 0
+      }
     }
   ];
   res.send(list);
