b45fd8a62b40a50e83c8e0512173d349fb4230d8
Marina Gosson <marina.gosson@gmail.com>
2020-01-29 15:33:23 -0300

    ajustes rota

--- app/routes/inventario.js
+++ app/routes/inventario.js
@@ -6,6 +6,28 @@ export const inventario = new Router();
 inventario.get("/", (req, res) => {
   res.send([
     {
+            id: 654987,
+            description: "Inicio do mes",
+            store_id: 1,
+            observation: "Lorem impsum",
+            ultima_contagem: 0,
+            tipo_contagem: "simples",
+            produtos_registrados: false,
+            produtos_avulsos: false,
+            produtos: ["1082", "1326", "1463", "1452", "1261", "1259", "7896864400321.0", "1176"]
+          },
+          {
+            id: 321654,
+            description: "Fim de mes",
+            store_id: 1,
+            observation: "Lorem impsum",
+            ultima_contagem: 0,
+            tipo_contagem: "simples",
+            produtos_registrados: false,
+            produtos_avulsos: false,
+            produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
+          },
+    {
       id: 654987,
       description: "Produto Avulso",
       store_id: 1,
@@ -52,32 +74,68 @@ inventario.get("/", (req, res) => {
   ]);
 });
 
-// inventario.get("/:id", (req, res) => {
-//   res.send([
-//     {
-//       id: 654987,
-//       description: "Inicio do mes",
-//       store_id: 1,
-//       observation: "Lorem impsum",
-//       ultima_contagem: 0,
-//       tipo_contagem: "simples",
-//       produtos_registrados: false,
-//       produtos_avulsos: false,
-//       produtos: ["1082", "1326", "1463", "1452", "1261", "1259", "7896864400321.0", "1176"]
-//     },
-//     {
-//       id: 321654,
-//       description: "Fim de mes",
-//       store_id: 1,
-//       observation: "Lorem impsum",
-//       ultima_contagem: 0,
-//       tipo_contagem: "simples",
-//       produtos_registrados: false,
-//       produtos_avulsos: false,
-//       produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
-//     }
-//   ]);
-// });
+inventario.get("/:id", (req, res) => {
+  var loja1 = 1;
+  var loja2 = 2;
+
+  var inventarioLoja1 = [
+    {
+      id: 65487,
+      description: "Inventario 1",
+      store_id: 1,
+      observation: "Permite produto avulso",
+      ultima_contagem: 0,
+      tipo_contagem: "simples",
+      produtos_registrados: false,
+      produtos_avulsos: true,
+      produtos: ["1082", "1326", "1463", "1452", "1261", "1259", "7896864400321.0", "1176"]
+    },
+    {
+      id: 321654,
+      description: "Inventario 2",
+      store_id: 1,
+      observation: "Não permite produto avulso",
+      ultima_contagem: 0,
+      tipo_contagem: "simples",
+      produtos_registrados: false,
+      produtos_avulsos: false,
+      produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
+    }
+  ];
+  var inventarioLoja2 = [
+    {
+      id: 12346,
+      description: "Produto Avulso",
+      store_id: 2,
+      observation: "Permite produto avulso",
+      ultima_contagem: 0,
+      tipo_contagem: "simples",
+      produtos_registrados: false,
+      produtos_avulsos: true,
+      produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
+    },
+    {
+      id: 987654,
+      description: "Inventario 2",
+      store_id: 2,
+      observation: "Não permite produto avulso",
+      ultima_contagem: 0,
+      tipo_contagem: "simples",
+      produtos_registrados: false,
+      produtos_avulsos: false,
+      produtos: ["1082", "1326", "1463", "1452", "1261", "1259", "7896864400321.0", "1176"]
+    }
+  ];
+
+  if(req.params.id == 1)
+    res.send(inventarioLoja1);
+
+  if(req.params.id == 2)
+    res.send(inventarioLoja2);
+
+  res.send([]);
+});
+
 // inventario.post("/", (req, res) => {
 //   res.send("TOO - implement!");
 // });
