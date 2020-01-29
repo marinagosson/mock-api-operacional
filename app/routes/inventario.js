1713c703bfc94f2c72e71e3da03ba0c9e298836d
Marina Gosson <marina.gosson@gmail.com>
2020-01-29 15:13:36 -0300

    alteracao no retorno de inventario

--- app/routes/inventario.js
+++ app/routes/inventario.js
@@ -7,7 +7,7 @@ inventario.get("/", (req, res) => {
   res.send([
     {
       id: 654987,
-      description: "Inicio do mes",
+      description: "Produto Avulso",
       store_id: 1,
       observation: "Lorem impsum",
       ultima_contagem: 0,
@@ -26,42 +26,64 @@ inventario.get("/", (req, res) => {
       produtos_registrados: false,
       produtos_avulsos: false,
       produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
+    },
+    {
+      id: 321654,
+      description: "Produto Avulso 1",
+      store_id: 1,
+      observation: "Lorem impsum",
+      ultima_contagem: 0,
+      tipo_contagem: "simples",
+      produtos_registrados: false,
+      produtos_avulsos: true,
+      produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
+    },
+    {
+      id: 321654,
+      description: "Produto Avulso 1",
+      store_id: 1,
+      observation: "Lorem impsum",
+      ultima_contagem: 0,
+      tipo_contagem: "simples",
+      produtos_registrados: false,
+      produtos_avulsos: true,
+      produtos: ["1082", "1326", "1463", "1452", "1261", "1259", "7896864400321.0", "1176"]
     }
   ]);
 });
 
-inventario.get("/:id", (req, res) => {
-  res.send([
-    {
-      id: 654987,
-      description: "Inicio do mes",
-      store_id: 1,
-      observation: "Lorem impsum",
-      ultima_contagem: 0,
-      tipo_contagem: "simples",
-      produtos_registrados: false,
-      produtos_avulsos: false,
-      produtos: ["1082", "1326", "1463", "1452", "1261", "1259", "7896864400321.0", "1176"]
-    },
-    {
-      id: 321654,
-      description: "Fim de mes",
-      store_id: 1,
-      observation: "Lorem impsum",
-      ultima_contagem: 0,
-      tipo_contagem: "simples",
-      produtos_registrados: false,
-      produtos_avulsos: false,
-      produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
-    }
-  ]);
-});
-inventario.post("/", (req, res) => {
-  res.send("TOO - implement!");
-});
-inventario.put("/:id", (req, res) => {
-  res.send("TOO - implement!");
-});
-inventario.delete("/:id", (req, res) => {
-  res.send("TOO - implement!");
-});
+// inventario.get("/:id", (req, res) => {
+//   res.send([
+//     {
+//       id: 654987,
+//       description: "Inicio do mes",
+//       store_id: 1,
+//       observation: "Lorem impsum",
+//       ultima_contagem: 0,
+//       tipo_contagem: "simples",
+//       produtos_registrados: false,
+//       produtos_avulsos: false,
+//       produtos: ["1082", "1326", "1463", "1452", "1261", "1259", "7896864400321.0", "1176"]
+//     },
+//     {
+//       id: 321654,
+//       description: "Fim de mes",
+//       store_id: 1,
+//       observation: "Lorem impsum",
+//       ultima_contagem: 0,
+//       tipo_contagem: "simples",
+//       produtos_registrados: false,
+//       produtos_avulsos: false,
+//       produtos: ["1201", "1337", "735201111995.0", "1365", "1472", "1464", "78935495", "929"]
+//     }
+//   ]);
+// });
+// inventario.post("/", (req, res) => {
+//   res.send("TOO - implement!");
+// });
+// inventario.put("/:id", (req, res) => {
+//   res.send("TOO - implement!");
+// });
+// inventario.delete("/:id", (req, res) => {
+//   res.send("TOO - implement!");
+// });
