d9bf457f128d79d399befeafcf9184e49845395d
Marina Gosson <marina.gosson@gmail.com>
2020-03-14 19:28:07 -0300

    fix retorno funcionario

--- app/routes/funcionario.js
+++ app/routes/funcionario.js
@@ -4,7 +4,7 @@ import { Funcionario } from "../models";
 export const funcionario = new Router();
 
 funcionario.get("/", (req, res) => {
-  if (req.headers.assinante == "joao")
+  if (req.headers.assinante == "joao") {
     res.send({
       id: 47,
       username: "João",
@@ -16,7 +16,8 @@ funcionario.get("/", (req, res) => {
       foto: "https://operacional.herokuapp.com/images/user.png",
       permissoes: ["PERM_API_CARGA_PDV_ACESSO"]
     });
-  if (req.headers.assinante == "maria")
+  }
+  if (req.headers.assinante == "maria") {
     res.send({
       id: 47,
       username: "Maria",
@@ -28,4 +29,5 @@ funcionario.get("/", (req, res) => {
       foto: "https://operacional.herokuapp.com/images/user.png",
       permissoes: ["PERM_API_CARGA_PDV_ACESSO"]
     });
+  }
 });
