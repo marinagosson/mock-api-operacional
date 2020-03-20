822b369aab1d41f0919c5d9b17449ff1360ec063
Marina Gosson <marina.gosson@gmail.com>
2020-03-20 17:44:42 -0300

    ajustes

--- app/routes/produto.js
+++ app/routes/produto.js
@@ -1,17 +1,25 @@
 import { Router } from "express";
-// import { Produto } from "../models";
-
-import { produtosAssinante1 } from "./utils/assinante"
-import { produtosAssinante2 } from "./utils/assinante"
 
 import { assinante1 } from "./utils/assinante";
 import { assinante2 } from "./utils/assinante"
+
+import { produtos as list1Assinante1 } from './utils/assinante1/newList1';
+import { produtos as list2Assinante1 } from './utils/assinante1/newList2';
+import { produtos as list3Assinante1 } from './utils/assinante1/newList3';
+
+import { produtos as list1Assinante2 } from './utils/assinante2/newList1';
+import { produtos as list2Assinante2 } from './utils/assinante2/newList2';
+import { produtos as list3Assinante2 } from './utils/assinante2/newList3';
 
 import { hasAssinante, resSendAssinanteNotFound } from "./utils/assinante";
 
 export const produto = new Router();
 
 produto.get("/", async (req, res) => {
+
+  var produtosAssinante1 = [...list1Assinante1, ...list2Assinante1, ...list3Assinante1];
+  var produtosAssinante2 = [...list1Assinante2, ...list2Assinante2, ...list3Assinante2];
+
   // const { page = 1 } = req.query;
   // const list = await Produto.query(qb =>
   //   qb.orderBy("descricao", "asc")
--- app/routes/utils/assinante.js
+++ app/routes/utils/assinante.js
@@ -10,18 +10,6 @@ export const resSendAssinanteNotFound = {
   message: "Assinante não enviado no header"
 };
 
-import { produtos as list1Assinante1 } from './assinante1/newList1';
-import { produtos as list2Assinante1 } from './assinante1/newList2';
-import { produtos as list3Assinante1 } from './assinante1/newList3';
-
-import { produtos as list1Assinante2 } from './assinante2/newList1';
-import { produtos as list2Assinante2 } from './assinante2/newList2';
-import { produtos as list3Assinante2 } from './assinante2/newList3';
-
-
-export const produtosAssinante1 = [...list1Assinante1, ...list2Assinante1, ...list3Assinante1];
-export const produtosAssinante2 = [...list1Assinante2, ...list2Assinante2, ...list3Assinante2];
-
 export const assinante1 = "marina";
 export const assinante2 = "natalia";
 
